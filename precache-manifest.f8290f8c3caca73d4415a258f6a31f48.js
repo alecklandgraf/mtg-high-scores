26f8218199cdad2a9edd41777be62b8c52e78b4a
Aleck Landgraf <aleck.landgraf@airbnb.com>
2020-05-22 12:00:14 -0700

    Updates

---
+++ precache-manifest.f8290f8c3caca73d4415a258f6a31f48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52b45dd2cb9e7dcb11e9ab25feedd04",
+    "url": "/mtg-high-scores/index.html"
+  },
+  {
+    "revision": "2aa551e99469040d2608",
+    "url": "/mtg-high-scores/static/css/main.b4d458b1.chunk.css"
+  },
+  {
+    "revision": "75a32be97e24916fe8f2",
+    "url": "/mtg-high-scores/static/js/2.2d713a89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mtg-high-scores/static/js/2.2d713a89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa551e99469040d2608",
+    "url": "/mtg-high-scores/static/js/main.a9f377a5.chunk.js"
+  },
+  {
+    "revision": "3f5edd13a60ebefc9246",
+    "url": "/mtg-high-scores/static/js/runtime-main.d31f5542.js"
+  },
+  {
+    "revision": "5a94f70d047866186a5624c41eff46b6",
+    "url": "/mtg-high-scores/static/media/ArcadeClassic.5a94f70d.otf"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mtg-high-scores/precache-manifest.f8290f8c3caca73d4415a258f6a31f48.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mtg-high-scores/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
